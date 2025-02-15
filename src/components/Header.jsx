38bc92f629d5d0c449982fdf70856b6f5e4778eb
Antonín Martykán <a.martykan@gmail.com>
2025-02-15 23:24:18 +0100

    merging done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-=======
 import globe from '../assets/globe.png'
-
->>>>>>> 681d1dee355a799e74d360542ddeed59e198dc45
 
 
 export default function Header() {
     return (
-<<<<<<< HEAD
-        <header>
-            <div className="header-container">
-                
-            </div>
-=======
+
         <header className="header">
             <img className="globe-img" src={globe} alt="Globe" />
             <span className="header-text">my travel journal.</span>
->>>>>>> 681d1dee355a799e74d360542ddeed59e198dc45
         </header>
     )
 }
